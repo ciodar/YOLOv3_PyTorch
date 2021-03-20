c793b092801269562029ab05176dab5d2f89a73d
Dario Cioni <dario.cioni@hermestrade.com>
2021-03-20 12:19:37 +0100

    dataset loading and tensor saving

--- density.py
+++ density.py
@@ -7,10 +7,16 @@ import torch
 from torchvision import datasets, transforms
 import dataset
 import tqdm
+import pathlib as pl
 
 def density(args):
     options = read_data_cfg(args.datacfg)
     train_file = options['train']
+    fm_file = pl.Path(args.det)
+    gt_file = (pl.Path.joinpath(fm_file.parent,'ground_truth_'+fm_file.name))
+    if not fm_file.parent.exists() or not gt_file.parent.exists():
+        raise Exception("Selected output path does not exist")
+
     m = Darknet(args.cfgfile)
     m.print_network()
     check_model = args.cfgfile.split('.')[-1]
@@ -31,24 +37,27 @@ def density(args):
         #print("Using device #", device_n, " (", get_device_name(cuda_device), ")")
     m.eval()
 
-    valid_dataset = dataset.listDataset(train_file, shape=(m.width, m.height),
+    valid_dataset = dataset.densityDataset(train_file, shape=(m.width, m.height),
                                         shuffle=False,
                                         transform=transforms.Compose([
                                             transforms.ToTensor(),
                                         ]))
     kwargs = {'num_workers': 4, 'pin_memory': True}
-    valid_batchsize = 2
+    valid_batchsize = 32
     valid_loader = torch.utils.data.DataLoader(
         valid_dataset, batch_size=valid_batchsize, shuffle=False, **kwargs)
     output = []
-
+    gt = []
     for count_loop, (data, target, org_w, org_h) in enumerate(tqdm.tqdm(valid_loader)):
         if use_cuda:
             data = data.cuda(cuda_device)
         output.append(m(data))
+        gt.append(target)
     output = torch.stack(output)
+    gt = torch.stack(gt)
     # n_batches,batch,depth,height,width
     torch.save(output,args.det)
+    torch.save(output,str(gt_file))
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -341,13 +341,30 @@ def read_truths_args(lab_path, min_box_scale):
         new_truths.append([truths[i][0], truths[i][1], truths[i][2], truths[i][3], truths[i][4]])
     return np.array(new_truths)
 
-def read_json_truths_args(annotations_df, min_box_scale,):
+def read_json_truths_args(annotations_df, min_box_scale):
     new_truths = []
     for a in annotations_df[4]:
         if a['bbox'][3]/annotations_df[2] < min_box_scale:
             continue
         new_truths.append([a['category_id'], a['bbox'][0]/annotations_df[2], a['bbox'][1]/annotations_df[3], a['bbox'][2]/annotations_df[2], a['bbox'][3]/annotations_df[3]])
     return np.array(new_truths)
+
+def read_truths_count(lab_path, min_box_scale):
+    truths = read_truths(lab_path)
+    count = 0
+    for i in range(truths.shape[0]):
+        if truths[i][3] < min_box_scale:
+            continue
+        count +=1
+    return count
+
+def read_json_truths_count(annotations_df, min_box_scale):
+    count = 0
+    for a in annotations_df[4]:
+        if a['bbox'][3] / annotations_df[2] < min_box_scale:
+            continue
+        count+=1
+    return count
 
 def load_class_names(namesfile):
     class_names = []
