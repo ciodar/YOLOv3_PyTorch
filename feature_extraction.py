2a60b2d68be81869db4787166bea369ee74afdfd
Dario Cioni <dario.cioni@hermestrade.com>
2021-04-08 16:55:36 +0200

    feature extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,5 @@
 from darknet import Darknet
 from utils import read_data_cfg
-import numpy as np
 import pathlib as pl
 import torch
 import dataset
@@ -8,12 +7,16 @@ import tqdm
 import cmdline
 from torchvision import transforms
 
-sets = ['train','valid']
-
 def feature_extraction(args):
     options = read_data_cfg(args.images)
+    assert args.set in ['train', 'valid', 'test']
+    eval_file = options[args.set]
     out_path = pl.Path(args.det)
-
+    fm_file = (pl.Path.joinpath(out_path.parent, out_path.name))
+    gt_file = (pl.Path.joinpath(out_path.parent, fm_file.stem + '_' + args.set + '_labels' + fm_file.suffix))
+    if not fm_file.parent.exists() or not gt_file.parent.exists():
+        raise Exception("Selected output path does not exist")
+    print("Saving features into %s,labels into %s" % (str(fm_file), str(gt_file)))
     m = Darknet(args.cfgfile)
     # m.print_network()
     check_model = args.cfgfile.split('.')[-1]
@@ -31,38 +34,37 @@ def feature_extraction(args):
         # print("Using device #", cuda_device, " (", get_device_name(cuda_device), ")")
     m.eval()
 
-    for set in sets:
-        fm_dir = (pl.Path.joinpath(out_path, set))
-        if not fm_dir.exists():
-            fm_dir.mkdir(parents=True, exist_ok=True)
-        print(f"Saving features into {str(fm_dir)}")
-        eval_file = options[set]
-        valid_dataset = dataset.densityDataset(eval_file, shape=(m.width, m.height),
-                                               shuffle=False,
-                                               transform=transforms.Compose([
-                                                   transforms.ToTensor(),
-                                               ]))
+    valid_dataset = dataset.densityDataset(eval_file, shape=(m.width, m.height),
+                                           shuffle=False,
+                                           transform=transforms.Compose([
+                                               transforms.ToTensor(),
+                                           ]))
 
-        kwargs = {'num_workers': 2, 'pin_memory': True}
-        assert args.bs > 0
-        valid_loader = torch.utils.data.DataLoader(
-            valid_dataset, batch_size=args.bs, shuffle=False, **kwargs)
+    fm = []
+    gt = []
+    kwargs = {'num_workers': 2, 'pin_memory': True}
+    assert args.bs > 0
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=args.bs, shuffle=False, **kwargs)
 
-        pbar = tqdm.tqdm(valid_loader)
-        with torch.no_grad():
-            for count_loop, (data, target, org_w, org_h) in enumerate(pbar):
-                if use_cuda:
-                    pbar.set_postfix({'GPU memory allocated': torch.cuda.memory_allocated(cuda_device) / (1024 * 1024)})
-                    # print("%5d|GPU memory allocated: %.3f MB"%(count_loop,(torch.cuda.memory_allocated(cuda_device) / (1024 * 1024))))
-                    data = data.to(cuda_device)
+    pbar = tqdm.tqdm(valid_loader)
+    with torch.no_grad():
+        for count_loop, (data, target, org_w, org_h) in enumerate(pbar):
+            if use_cuda:
+                pbar.set_postfix({'GPU memory allocated': torch.cuda.memory_allocated(cuda_device) / (1024 * 1024)})
+                # print("%5d|GPU memory allocated: %.3f MB"%(count_loop,(torch.cuda.memory_allocated(cuda_device) / (1024 * 1024))))
+                data = data.cuda(cuda_device)
+            output = m(data).clone().detach()
+            fm.append(output)
+            gt.append(target.clone().detach())
+            del data, target, output
+    # n_batches,batch,depth,height,width
+    fm = torch.stack(fm).reshape(len(valid_dataset), 1792, 8, 10)
+    gt = gt.stack(gt).reshape(len(valid_dataset))
 
-                output = m(data).cpu().numpy()
-                np.save(str(pl.Path.joinpath(fm_dir,pl.Path(valid_dataset.get_image(count_loop)).stem)),output)
-                with open(pl.Path.joinpath(fm_dir,pl.Path(valid_dataset.get_image(count_loop)).stem+'.txt'),'w') as f:
-                    f.write(str(target.item()))
+    torch.save(fm, str(fm_file))
+    torch.save(gt, str(gt_file))
 
-                del data, target, output
-        # n_batches,batch,depth,height,width
 
 if __name__ == '__main__':
     args = cmdline.arg_parse()
